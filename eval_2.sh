ab26f1f31a27ca212a0c997a36109008871e9a4a
xiulinyang <xiulin.yang.compling@gmail.com>
2025-11-24 22:42:45 -0500

    update eval

--- eval_2.sh
+++ eval_2.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-python eval_bench.py xiulinyang/dynamic_only_96 --eval_dataset posh
-python eval_bench.py xiulinyang/dynamic_only_128 --eval_dataset posh
-python eval_bench.py xiulinyang/dynamic_only_0 --eval_dataset posh
+python eval_bench.py xiulinyang/dynamic_dyck_96 --eval_dataset posh
+python eval_bench.py xiulinyang/dynamic_dyck_128 --eval_dataset posh
+python eval_bench.py xiulinyang/dynamic_dyck_0 --eval_dataset posh
 
-python eval_bench.py xiulinyang/dynamic_only_96 --eval_dataset zorro
-python eval_bench.py xiulinyang/dynamic_only_128 --eval_dataset zorro
-python eval_bench.py xiulinyang/dynamic_only_0 --eval_dataset zorro
+python eval_bench.py xiulinyang/dynamic_dyck_96 --eval_dataset zorro
+python eval_bench.py xiulinyang/dynamic_dyck_128 --eval_dataset zorro
+python eval_bench.py xiulinyang/dynamic_dyck_0 --eval_dataset zorro
 
 
-python eval_bench.py xiulinyang/dynamic_only_96 --eval_dataset scamp_plausible
-python eval_bench.py xiulinyang/dynamic_only_128 --eval_dataset scamp_plausible
-python eval_bench.py xiulinyang/dynamic_only_0 --eval_dataset scamp_plausible
+python eval_bench.py xiulinyang/dynamic_dyck_96 --eval_dataset scamp_plausible
+python eval_bench.py xiulinyang/dynamic_dyck_128 --eval_dataset scamp_plausible
+python eval_bench.py xiulinyang/dynamic_dyck_0 --eval_dataset scamp_plausible
 
-python eval_bench.py xiulinyang/dynamic_only_96 --eval_dataset blimp
-python eval_bench.py xiulinyang/dynamic_only_128 --eval_dataset blimp
-python eval_bench.py xiulinyang/dynamic_only_0 --eval_dataset blimp
+python eval_bench.py xiulinyang/dynamic_dyck_96 --eval_dataset blimp
+python eval_bench.py xiulinyang/dynamic_dyck_128 --eval_dataset blimp
+python eval_bench.py xiulinyang/dynamic_dyck_0 --eval_dataset blimp
 
 #python eval_bench.py xiulinyang/dynamic_dyck_chunk_96 --eval_dataset blimp
 #python eval_bench.py xiulinyang/dynamic_dyck_chunk_128 --eval_dataset blimp
