589690729b7cc134cd0d871877fddcc0f5107d34
xiulinyang <xiulin.yang.compling@gmail.com>
2025-11-24 22:45:52 -0500

    update eval

--- eval_2.sh
+++ eval_2.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-python eval_bench.py xiulinyang/dynamic_dyck_96 --eval_dataset posh
-python eval_bench.py xiulinyang/dynamic_dyck_128 --eval_dataset posh
-python eval_bench.py xiulinyang/dynamic_dyck_0 --eval_dataset posh
+python eval_bench.py xiulinyang/linear_dyck_96 --eval_dataset posh
+python eval_bench.py xiulinyang/linear_dyck_128 --eval_dataset posh
+python eval_bench.py xiulinyang/linear_dyck_0 --eval_dataset posh
 
-python eval_bench.py xiulinyang/dynamic_dyck_96 --eval_dataset zorro
-python eval_bench.py xiulinyang/dynamic_dyck_128 --eval_dataset zorro
-python eval_bench.py xiulinyang/dynamic_dyck_0 --eval_dataset zorro
+python eval_bench.py xiulinyang/linear_dyck_96 --eval_dataset zorro
+python eval_bench.py xiulinyang/linear_dyck_128 --eval_dataset zorro
+python eval_bench.py xiulinyang/linear_dyck_0 --eval_dataset zorro
 
 
-python eval_bench.py xiulinyang/dynamic_dyck_96 --eval_dataset scamp_plausible
-python eval_bench.py xiulinyang/dynamic_dyck_128 --eval_dataset scamp_plausible
-python eval_bench.py xiulinyang/dynamic_dyck_0 --eval_dataset scamp_plausible
+python eval_bench.py xiulinyang/linear_dyck_96 --eval_dataset scamp_plausible
+python eval_bench.py xiulinyang/linear_dyck_128 --eval_dataset scamp_plausible
+python eval_bench.py xiulinyang/linear_dyck_0 --eval_dataset scamp_plausible
 
-python eval_bench.py xiulinyang/dynamic_dyck_96 --eval_dataset blimp
-python eval_bench.py xiulinyang/dynamic_dyck_128 --eval_dataset blimp
-python eval_bench.py xiulinyang/dynamic_dyck_0 --eval_dataset blimp
+python eval_bench.py xiulinyang/linear_dyck_96 --eval_dataset blimp
+python eval_bench.py xiulinyang/linear_dyck_128 --eval_dataset blimp
+python eval_bench.py xiulinyang/linear_dyck_0 --eval_dataset blimp
 
-#python eval_bench.py xiulinyang/dynamic_dyck_chunk_96 --eval_dataset blimp
-#python eval_bench.py xiulinyang/dynamic_dyck_chunk_128 --eval_dataset blimp
-#python eval_bench.py xiulinyang/dynamic_dyck_chunk_0 --eval_dataset blimp
+#python eval_bench.py xiulinyang/linear_dyck_chunk_96 --eval_dataset blimp
+#python eval_bench.py xiulinyang/linear_dyck_chunk_128 --eval_dataset blimp
+#python eval_bench.py xiulinyang/linear_dyck_chunk_0 --eval_dataset blimp
 
-#python eval_bench.py xiulinyang/dynamic_chunk_96 --eval_dataset posh
-#python eval_bench.py xiulinyang/dynamic_chunk_128 --eval_dataset posh
+#python eval_bench.py xiulinyang/linear_chunk_96 --eval_dataset posh
+#python eval_bench.py xiulinyang/linear_chunk_128 --eval_dataset posh
 
-#python eval_bench.py xiulinyang/dynamic_chunk_96 --eval_dataset zorro
-#python eval_bench.py xiulinyang/dynamic_chunk_128 --eval_dataset zorro
+#python eval_bench.py xiulinyang/linear_chunk_96 --eval_dataset zorro
+#python eval_bench.py xiulinyang/linear_chunk_128 --eval_dataset zorro
 
-#python eval_bench.py xiulinyang/dynamic_chunk_96 --eval_dataset scamp_plausible
-#python eval_bench.py xiulinyang/dynamic_chunk_128 --eval_dataset scamp_plausible
+#python eval_bench.py xiulinyang/linear_chunk_96 --eval_dataset scamp_plausible
+#python eval_bench.py xiulinyang/linear_chunk_128 --eval_dataset scamp_plausible
 
-#python eval_bench.py xiulinyang/dynamic_chunk_96 --eval_dataset blimp
-#python eval_bench.py xiulinyang/dynamic_chunk_128 --eval_dataset blimp
+#python eval_bench.py xiulinyang/linear_chunk_96 --eval_dataset blimp
+#python eval_bench.py xiulinyang/linear_chunk_128 --eval_dataset blimp
